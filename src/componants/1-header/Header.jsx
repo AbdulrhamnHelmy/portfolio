378c520f64cbd1fa0c1dc73fb8f5228e342f1bd2
AbdulrhamnHelmy <abdulrahmanhelmy27@gmail.com>
2024-01-05 16:17:34 +0200

    first commit

--- src/componants/1-header/Header.jsx
+++ src/componants/1-header/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
             <a href="#home">Home</a>
           </li>
           <li>
-            <a href="#about">About me</a>
+            <a href="#about">About</a>
           </li>
           <li>
             <a href="#skills">Skills</a>
@@ -41,7 +41,7 @@ const Header = () => {
             <a href="#main">Projects</a>
           </li>
           <li>
-            <a href="#contact">Contact us</a>
+            <a href="#contact">Contact</a>
           </li>
         </ul>
       </nav>
@@ -78,7 +78,7 @@ const Header = () => {
               <a href="#home">Home</a>
             </li>
             <li>
-              <a href="#about">About me</a>
+              <a href="#about">About</a>
             </li>
             <li>
               <a href="#skills">Skills</a>
@@ -87,7 +87,7 @@ const Header = () => {
               <a href="#main">Projects</a>
             </li>
             <li>
-              <a href="#contact">Contact us</a>
+              <a href="#contact">Contact </a>
             </li>
           </ul>
         </div>
